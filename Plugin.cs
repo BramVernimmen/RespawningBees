e86439eb7359dcd3912b78649f5a69f4e0319471
Bram Vernimmen <vernimmenbram@gmail.com>
2024-03-11 17:33:01 +0100

    Initial project commit

---
+++ Plugin.cs
@@ -0,0 +1,17 @@
+﻿using BepInEx;
+using HarmonyLib;
+using System.Reflection;
+
+namespace RespawningBees
+{
+    [BepInPlugin("Pro192.uch.RespawningBees", "Respawning Bees", "1.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+            Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# RespawningBees
- Mod for Ultimate Chicken Horse
+# Respawning Bees
+ A mod for Ultimate Chicken Horse that allows the bees to respawn after they have killed their target.
