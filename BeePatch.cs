29bcf0d32fa7cfec6988989eb017af15e98f8133
Bram Vernimmen <vernimmenbram@gmail.com>
2024-03-11 17:33:26 +0100

    Patched BeeSwarm

---
+++ BeePatch.cs
@@ -0,0 +1,21 @@
+﻿using BepInEx.Logging;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RespawningBees
+{
+    [HarmonyPatch(typeof(BeeSwarm), "Disappear")]
+    public class BeePatch
+    {
+        [HarmonyPostfix]
+        public static void DisappearPostFix(BeeSwarm __instance)
+        {
+            var logSource = new ManualLogSource("RespawnableBeeHive");
+            BepInEx.Logging.Logger.Sources.Add(logSource);
+            logSource.LogInfo("Swarm has disappeared");
+            __instance.HomeHive.Reset();
+        }
+    }
+}
